d669f0b7fa32f147d4f3869cadabe2b13afe386c
liorlr22 <s9077@rehovot.ort.org.il>
2023-04-19 12:18:08 +0300

    updating code

---
+++ new_socket_files/client-new.py
@@ -0,0 +1,36 @@
+import os
+import socket
+import music21
+
+
+def receive_files(client_socket, directory):
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        filename = data.decode().strip()
+        filepath = os.path.join(directory, filename)
+        print(f'Receiving file {filename}')
+        with open(filepath, 'wb') as f:
+            while True:
+                filedata = client_socket.recv(1024)
+                if not filedata:
+                    break
+                f.write(filedata)
+
+
+def main():
+    # Set up client socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 8888))
+
+    # Receive files from server
+    directory = 'receive/'
+    receive_files(client_socket, directory)
+
+    # Close client connection
+    client_socket.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ new_socket_files/server-new.py
@@ -0,0 +1,40 @@
+import os
+import socket
+import tkinter
+
+
+def send_files(client_socket, directory):
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        with open(filepath, 'rb') as f:
+            client_socket.sendall(filename.encode() + b'\n')
+            while True:
+                filedata = f.read(1024)
+                if not filedata:
+                    break
+                client_socket.sendall(filedata)
+
+
+def main():
+    # Set up server socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', 8888))
+    server_socket.listen(1)
+    print('Server started, waiting for connections...')
+
+    while True:
+        # Accept incoming client connection
+        client_socket, client_address = server_socket.accept()
+        print(f'Client connected from {client_address}')
+
+        # Send files to client
+        directory = '../parts/opt-merged'
+        send_files(client_socket, directory)
+
+        # Close client connection
+        client_socket.close()
+        print(f'Connection to {client_address} closed')
+
+
+if __name__ == '__main__':
+    main()
