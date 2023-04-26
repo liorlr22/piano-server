511ed2c9c93384b49f64a1a726db6be922cdcaa5
liorlr22 <s9077@rehovot.ort.org.il>
2023-04-26 14:13:37 +0300

    added tkinter window to server

--- new_socket_files/server-new.py
+++ new_socket_files/server-new.py
@@ -1,7 +1,8 @@
 import os
 import socket
 from parts import GetLength
-import tkinter
+import tkinter as tk
+from tkinter import messagebox
 
 
 def send_files(client_socket, directory):
@@ -14,6 +15,56 @@ def send_files(client_socket, directory):
                 if not filedata:
                     break
                 client_socket.sendall(filedata)
+
+
+def open_window():
+    # Check if MIDI directory exists and is not empty
+    midi_dir = "../resources/midi/"
+    if not os.path.exists(midi_dir):
+        messagebox.showerror("Error", f"MI  DI directory '{midi_dir}' not found.")
+        exit()
+    midi_files = os.listdir(midi_dir)
+    if not midi_files:
+        messagebox.showerror("Error", f"No MIDI files found in directory '{midi_dir}'.")
+        exit()
+
+    # Create a tkinter window
+    window = tk.Tk()
+    window.title("Piano Server")
+    window.resizable(width=False, height=False)
+
+    # Create a frame in the center
+    frame = tk.Frame(window)
+    frame.pack(fill=tk.BOTH, expand=True)
+
+    # Add title label to the frame
+    title = tk.Label(frame, text="Piano Server", font=("Arial", 24))
+    title.grid(row=0, column=0, pady=20, sticky="n", columnspan=len(midi_files))
+
+    # Add label for the number of connected clients
+    num_clients_label = tk.Label(frame, text="Clients: 0", font=("Arial", 16))
+    num_clients_label.grid(row=1, column=0, pady=10, sticky="w", columnspan=5)
+
+    # Calculate number of columns needed
+    num_files = len(midi_files)
+    max_rows = 10  # maximum number of rows per column
+    max_cols = 5  # maximum number of columns
+    num_cols = min(max(1, num_files // max_rows), max_cols)
+    num_rows = (num_files + num_cols - 1) // num_cols
+
+    # Add MIDI buttons to the frame
+    for i in range(num_files):
+        btn = tk.Button(frame, text=midi_files[i].rstrip(".mid"), padx=10,
+                        command=lambda name=midi_files[i]: on_button_click(name),
+                        width=15, wraplength=100, height=2)
+        btn.grid(row=(i // num_cols) + 1, column=i % num_cols, pady=5)
+
+    # Run the main loop
+    window.mainloop()
+
+
+def on_button_click(name):
+    pass
 
 
 def main():
