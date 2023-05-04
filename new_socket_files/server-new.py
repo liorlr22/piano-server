4f0c42f4cc9a0d6e56c21341c8c0255df16da28c
liorlr22 <s9077@rehovot.ort.org.il>
2023-05-05 00:38:25 +0300

    rewriting the server file, adding gui

--- new_socket_files/server-new.py
+++ new_socket_files/server-new.py
@@ -1,127 +1,84 @@
-try:
-    import os
-    import socket
-    from mido import MidiFile
-    import tkinter as tk
-    from tkinter import messagebox
-    from parts.CropToSeconds import crop
-except ModuleNotFoundError as e:
-    print(f"Error: {e}")
-
-clients = []
+import socket
+import threading
+from tkinter import messagebox
+import customtkinter
+import os
 
 
-def get_midi_length(path):
-    mid = MidiFile(path)
-    return mid.length
+def change_appearance_mode_event(new_appearance_mode: str):
+    customtkinter.set_appearance_mode(new_appearance_mode)
 
 
-def cumulative_sum(lst, index):
-    if index >= len(lst):
-        return None
-
-    return sum(lst[:index + 1])
+def on_button_click(file_name: str):
+    print(file_name)
 
 
-def send_files(client_socket, directory):
-    print("sending")
-    # Count the number of files in the directory
-    file_count = len(os.listdir(directory))
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
-    # Divide the file count by the number of clients
-    files_per_client = file_count // len(clients)
+        # configure window
+        self.title("Remote Pianist")
+        self.geometry(f"{1100}x{580}")
+        self.iconbitmap("../resources/pictures/electric.ico")
 
-    # Iterate over the clients
-    for i, client_socket in enumerate(clients):
-        # Calculate the starting and ending indices for this client's files
-        start_index = i * files_per_client
-        end_index = (i + 1) * files_per_client
+        # configure grid layout (4x4)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        # Iterate over the files in the directory and send the appropriate ones to this client
-        for j, filename in enumerate(os.listdir(directory)):
-            if start_index <= j < end_index:
-                filepath = os.path.join(directory, filename)
-                with open(filepath, 'rb') as f:
-                    client_socket.sendall(filename.encode() + b'\n')
-                    while True:
-                        filedata = f.read(1024)
-                        if not filedata:
-                            break
-                        client_socket.sendall(filedata)
+        self.clients_connected_count = 0
+        # Check if MIDI directory exists and is not empty
+        self.midi_dir = "../resources/midi/"
+        if not os.path.exists(self.midi_dir):
+            messagebox.showerror("Error", f"MIDI directory '{self.midi_dir}' not found.")
+            exit()
+        self.midi_files = os.listdir(self.midi_dir)
+        if not self.midi_files:
+            messagebox.showerror("Error", f"No MIDI files found in directory '{self.midi_dir}'.")
+            exit()
 
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, columnspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        self.title_label = customtkinter.CTkLabel(self.sidebar_frame, text="Remote Pianist", anchor="center",
+                                                  font=("Ariel", 32))
+        self.title_label.grid(row=2, column=0, padx=20, pady=(10, 10))
+        self.clients_connected_label = customtkinter.CTkLabel(self.sidebar_frame,
+                                                              text=f"Clients: {self.clients_connected_count}",
+                                                              anchor="center", font=("Ariel", 25))
+        self.clients_connected_label.grid(row=3, column=0, padx=20, pady=(10, 10))
 
-def open_window():
-    # Check if MIDI directory exists and is not empty
-    midi_dir = "../resources/midi/"
-    if not os.path.exists(midi_dir):
-        messagebox.showerror("Error", f"MI  DI directory '{midi_dir}' not found.")
-        exit()
-    midi_files = os.listdir(midi_dir)
-    if not midi_files:
-        messagebox.showerror("Error", f"No MIDI files found in directory '{midi_dir}'.")
-        exit()
+        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w",
+                                                            font=("Ariel", 15))
+        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_option_menu = customtkinter.CTkOptionMenu(self.sidebar_frame,
+                                                                       values=["Light", "Dark", "System"],
+                                                                       command=change_appearance_mode_event)
+        self.appearance_mode_option_menu.grid(row=6, column=0, padx=20, pady=(10, 10))
 
-    # Create a tkinter window
-    window = tk.Tk()
-    window.title("Piano Server")
-    window.resizable(width=False, height=False)
+        # set default values
+        self.appearance_mode_option_menu.set("System")
 
-    # Create a frame in the center
-    frame = tk.Frame(window)
-    frame.pack(fill=tk.BOTH, expand=True)
+        self.button_frame = customtkinter.CTkFrame(self)
+        self.button_frame.grid(row=0, column=2, padx=(20, 20), pady=(20, 0), sticky="nsew")
 
-    # Add title label to the frame
-    title = tk.Label(frame, text="Piano Server", font=("Arial", 24))
-    title.grid(row=0, column=0, pady=20, sticky="n", columnspan=len(midi_files))
+        # Calculate number of columns needed
+        self.num_files = len(self.midi_files)
+        self.max_rows = 10  # maximum number of rows per column
+        self.max_cols = 5  # maximum number of columns
+        self.num_cols = min(max(1, self.num_files // self.max_rows), self.max_cols)
+        self.num_rows = (self.num_files + self.num_cols - 1) // self.num_cols
 
-    # Add label for the number of connected clients
-    num_clients_label = tk.Label(frame, text="Clients: 0", font=("Arial", 16))
-    num_clients_label.grid(row=1, column=0, pady=10, sticky="w", columnspan=5)
-
-    # Calculate number of columns needed
-    num_files = len(midi_files)
-    max_rows = 10  # maximum number of rows per column
-    max_cols = 5  # maximum number of columns
-    num_cols = min(max(1, num_files // max_rows), max_cols)
-    num_rows = (num_files + num_cols - 1) // num_cols
-
-    # Add MIDI buttons to the frame
-    for i in range(num_files):
-        btn = tk.Button(frame, text=midi_files[i].rstrip(".mid"), padx=10,
-                        command=lambda name=midi_files[i], cs=clients[0][0]: on_button_click(name, cs),
-                        width=15, wraplength=100, height=2)
-        btn.grid(row=(i // num_cols) + 1, column=i % num_cols, pady=5)
-
-    # Run the main loop
-    window.mainloop()
-
-
-def on_button_click(name, client_socket):
-    crop(f"../resources/midi/{name}")
-    # Send files to client
-    directory = '../parts/opt-merged'
-    send_files(client_socket, directory)
-
-
-def main():
-    # Set up server socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 8888))
-    server_socket.listen()
-    print('Server started, waiting for connections...')
-
-    while True:
-        # Accept incoming client connection
-        client_socket, client_address = server_socket.accept()
-        print(f'Client connected from {client_address}')
-        clients.append((client_socket, client_address))
-
-        open_window()
-
-        # Close client connection
-        client_socket.close()
-        print(f'Connection to {client_address} closed')
+        # Add MIDI buttons to the frame
+        for i in range(self.num_files):
+            btn = customtkinter.CTkButton(self.button_frame, text=self.midi_files[i].rstrip(".mid").capitalize(),
+                                          command=lambda name=self.midi_files[i]: on_button_click(name))
+            btn.grid(row=(i // self.num_cols) + 1, column=i % self.num_cols, pady=5, padx=10, sticky="nsew")
 
 
 if __name__ == '__main__':
-    main()
+    customtkinter.set_appearance_mode("System")
+    customtkinter.set_default_color_theme("blue")
+    app = App()
+    app.mainloop()
