301327a45e8cc8fd1d741623a1a39e459c63ecb9
liorlr22 <s9077@rehovot.ort.org.il>
2023-05-27 15:12:45 +0300

    remove todo from main client

--- main-client.py
+++ main-client.py
@@ -1,12 +1,5 @@
 from lib.visuals import ClientApp
 
-
-# TODO: Add Play MIDI
-#     - Create Midi FILE
-#     - add midi track with same properties
-#     - add midi message(s)
-#     - Play Midi FILE
-
 class Main:
     def __init__(self) -> None:
         self.app = ClientApp()
